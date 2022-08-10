5fbd1452c7c28055ed4a7b3fb5eaf9c3d6dae840
Mingu Kim <mingu757377@gmail.com>
2022-08-11 01:39:09 +0900

    To Do List

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [toDo, setToDo] = useState('')
+  const [toDos, setToDos] = useState([])
+  const onChange = (e) => setToDo(e.target.value)
+  const onSubmit = (e) => {
+    e.preventDefault()
+    if (toDo === '') {
+      return
+    }
+    setToDos((currentArray) => [toDo, ...currentArray])
+    setToDo('')
+  }
+  // console.log(toDos)
+  console.log(toDos.map((task, index) => <li key={index}>{task}</li>))
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To Do ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Add task"
+        />
+        <button>Add</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
